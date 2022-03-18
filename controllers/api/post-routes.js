a3dc26b5a21cc3122df7cf125421fb7746dcf6d8
George Cheng <sparklyspuds@gmail.com>
2022-03-19 00:00:18 +0800

    get all endpoint added

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,3 +1,31 @@
 const router = require("express").Router();
 const{User,Post,Comment}=require("../../models");
+const isLogin= require("../../utils/auth");
 
+router.get("/",(req,res)=>{
+Post.findAll({
+    attributes:["id","content","title","created_at"],
+    order:[["created_at","DESC"]],
+    include:[
+        {
+            model:Comment,
+            attributes:["id","comment_text","post_id","user_id","created_at"],
+            include:{
+                model:User,
+                attributes:["username"],
+            },
+
+        },{
+            model:User,
+            attributes:["username"],
+        }
+    ]
+}).then((posts)=>{
+    res.json(posts)
+    .catch((err)=>{
+        console.log(err);
+        res.status(500).json(err)
+    })
+})
+
+})
