ffcc2056ec0a9c282cd9db5d55bd6e71401d68ea
George Cheng <sparklyspuds@gmail.com>
2022-03-18 08:43:44 +0800

    checkPassword ,encrypt , model built

---
+++ models/User.js
@@ -0,0 +1,63 @@
+const { Model, Datatype } = require("sequelize");
+const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
+const { beforeCreate } = require("../../../Tutor/week14/tech-blog/models/User");
+
+class User extends Model {
+  // set up method to run on instance data checking userpassword
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        // ensure one email can only sign up once
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [6],
+      },
+    },
+  },
+  {
+    hooks: {
+      async beforeCreate(newUser) {
+        newUser.password = await bcrypt.hash(newUser, 10);
+        return newUser;
+      },
+    },
+    // pass imported sequelize connection
+    sequelize,
+    // don't add the timestamp attributes
+    timestamps: false,
+    // Model tableName will be the same as the model name
+    freezeTableName: true,
+    // Will automatically set field option for all attributes to snake cased name.
+    underscore: true,
+    // make it so our model name stays lowercase in the database
+    modelName: "user",
+  }
+);
+
+module.exports = User;
