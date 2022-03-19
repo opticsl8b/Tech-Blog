77ddb5063d32a9b8764e74cbd663ba7b926f1287
George Cheng <sparklyspuds@gmail.com>
2022-03-19 21:13:16 +0800

    path-router assigned, background img added

--- controllers/index.js
+++ controllers/index.js
@@ -1,2 +1,17 @@
-const router=require("express").Router();
-const homeRoutes=require("./home-routes")
+const router = require("express").Router();
+// collecting endpoints for homepage, dashboard, and API
+const homeRoutes = require("./home-routes.js");
+const dashboardRoutes = require("./dashboard-routes.js");
+const apiRoutes = require("./api");
+
+// relationshiping the path-routes
+router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
+router.use("/dashboard", dashboardRoutes);
+
+// safety guard responding requested path doesnt exist with 404
+router.use((req, res) => {
+  res.status(404).json({ message: "Incorrect Route Provide" }).end();
+});
+
+module.exports = router;
